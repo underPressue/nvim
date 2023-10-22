182d6354a464a406a49118c14d21d1eeb370e526
underPressure <elrocie@gmail.com>
2023-10-23 00:05:33 +0600

    initial commit

---
+++ init.lua
@@ -0,0 +1,21 @@
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+--
+_G.get_visual_selection = function()
+  local s_start = vim.fn.getpos("'<")
+  local s_end = vim.fn.getpos("'>")
+  local n_lines = math.abs(s_end[2] - s_start[2]) + 1
+  local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
+  lines[1] = string.sub(lines[1], s_start[3], -1)
+  if n_lines == 1 then
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
+  else
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
+  end
+  return table.concat(lines, '\n')
+end
---
+++ mappings.lua
@@ -0,0 +1,27 @@
+---@type MappingsTable
+local M = {}
+
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<leader>S"] = {'<cmd>lua require("spectre").toggle()<CR>', "Toggle Spectre" },
+    ["<leader>sw"] = {'<cmd>lua require("spectre").open_visual({select_word=true})<CR>', "Search current word" },
+    ["<leader>sp"] = {'<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', "Search on current file" },
+  },
+  v = {
+    [">"] = { ">gv", "indent" },
+    ["<leader>sw"] = {'<esc><cmd>lua require("spectre").open_visual()<CR>', "Search current word" },
+    ["<leader>fe"] = {
+      ':lua require("telescope.builtin").grep_string({ search =  _G.get_visual_selection() })<CR>',
+      "indent",
+      opts = { noremap = true, silent = true },
+    },
+  },
+  x = {
+  }
+}
+
+-- more keybinds!
+
+return M
