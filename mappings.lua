28a82cb5fbcefc9ff2bc2175c18fb316bf497fca
underPressure <elrocie@gmail.com>
2023-10-24 09:51:40 +0600

    spectre current file search remapped

--- mappings.lua
+++ mappings.lua
@@ -7,7 +7,7 @@ M.general = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<leader>S"] = {'<cmd>lua require("spectre").toggle()<CR>', "Toggle Spectre" },
     ["<leader>sw"] = {'<cmd>lua require("spectre").open_visual({select_word=true})<CR>', "Search current word" },
-    ["<leader>sp"] = {'<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', "Search on current file" },
+    ["<leader>sc"] = {'<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', "Search on current file" },
   },
   v = {
     [">"] = { ">gv", "indent" },
