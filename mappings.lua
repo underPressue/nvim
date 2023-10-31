ca0983e484d2ab943ac0737b66d615ff9eee404a
underPressure <elrocie@gmail.com>
2023-10-31 10:08:05 +0600

    new shortcuts

--- mappings.lua
+++ mappings.lua
@@ -7,9 +7,11 @@ M.general = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<C-j>"] = { "<C-e>", "Screen down", opts = { nowait = true } },
     ["<C-k>"] = { "<C-y>", "Screen up", opts = { nowait = true } },
+    -- Search with Spectre
     ["<leader>S"] = {'<cmd>lua require("spectre").toggle()<CR>', "Toggle Spectre" },
     ["<leader>sw"] = {'<cmd>lua require("spectre").open_visual({select_word=true})<CR>', "Search current word" },
     ["<leader>sc"] = {'<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', "Search on current file" },
+    -- switch between buffers
     ["<S-l>"] = {
       function()
         require("nvchad.tabufline").tabuflineNext()
@@ -27,7 +29,7 @@ M.general = {
   v = {
     [">"] = { ">gv", "indent" },
     ["<leader>sw"] = {'<esc><cmd>lua require("spectre").open_visual()<CR>', "Search current word" },
-    ["<leader>fe"] = {
+    ["<leader>fw"] = {
       ':lua require("telescope.builtin").grep_string({ search =  _G.get_visual_selection() })<CR>',
       "indent",
       opts = { noremap = true, silent = true },
